970b1438a2d65d3ed156858b02d995b777a5448c
Sonia William <williamlaw2017@outlook.com>
2023-03-23 12:00:47 +0800

    add function to play one round

--- top-rps.js
+++ top-rps.js
@@ -12,5 +12,22 @@ function getComputerChoices() {
             return = "Scissors";
             break;
     }
+}
+    function playRound (playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            roundResult.textContent = "It's a tie!";
+        }else if (playerSelection == "rock" && computerSelection == "scissors") {
+            roundResult.textContent = "You win! Rock beats scissors!";
+        }else if (playerSelection == "rock" && computerSelection == "paper") {
+            roundResult.textContent = "You lose! Paper beats rock!";
+        }else if (playerSelection == "paper" && computerSelection == "scissors") {
+            roundResult.textContent = "You lose! Scissors beats paper!";
+        }else if (playerSelection == "paper" && computerSelection == "rock") {
+            roundResult.textContent = "You win! Paper beats rock!";
+        }else if (playerSelection == "scissors" && computerSelection == "paper") {
+            roundResult.textContent = "You win! Scissors beats paper!";
+        }else if (playerSelection == "scissors" && computerSelection == "rock") {
+            roundResult.textContent = "You lose! Rock beats scissors!";
+        
     }
-
+    }
