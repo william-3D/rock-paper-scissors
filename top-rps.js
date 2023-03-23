48b1abc3ba24eed3c74636b6cc3ac43016e1d4f7
Sonia William <williamlaw2017@outlook.com>
2023-03-23 12:10:51 +0800

    edit getComputerChoice and playRound

--- top-rps.js
+++ top-rps.js
@@ -1,15 +1,15 @@
-function getComputerChoices() {
+function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     
     switch(randomNumber){
         case 0:
-            return = "Rock";
+            return "Rock";
             break;
         case 1:
-            return = "Paper";
+            return "Paper";
             break;
         case 2:
-            return = "Scissors";
+            return "Scissors";
             break;
     }
 }
@@ -20,16 +20,21 @@ function getComputerChoices() {
             roundResult.textContent = "It's a tie!";
         }else if (playerSelection == "rock" && computerSelection == "scissors") {
             roundResult.textContent = "You win! Rock beats scissors!";
+            return "Success!"
         }else if (playerSelection == "rock" && computerSelection == "paper") {
             roundResult.textContent = "You lose! Paper beats rock!";
+            return "Epic fail"
         }else if (playerSelection == "paper" && computerSelection == "scissors") {
             roundResult.textContent = "You lose! Scissors beats paper!";
+            return "Epic fail"
         }else if (playerSelection == "paper" && computerSelection == "rock") {
             roundResult.textContent = "You win! Paper beats rock!";
+            return "Success!"
         }else if (playerSelection == "scissors" && computerSelection == "paper") {
             roundResult.textContent = "You win! Scissors beats paper!";
+            return "Success!"
         }else if (playerSelection == "scissors" && computerSelection == "rock") {
             roundResult.textContent = "You lose! Rock beats scissors!";
-            
+            return "Epic fail"
     }
     }
