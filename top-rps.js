8fcd63f172c3f5594e1643bc587d523a92089515
Sonia William <williamlaw2017@outlook.com>
2023-04-03 11:31:36 +0800

    complete playRound function

--- top-rps.js
+++ top-rps.js
@@ -1,17 +1,19 @@
-sign = window.prompt("Rock, paper or scissors?");
+// sign = window.prompt("Rock, paper or scissors?");
 
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
-const roundResult = document.getElementById("result");
-let userChoice; // globally saved so can be accessed wherever
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button"); // to pick out created buttons
 
-// to pick out created buttons
-const possibleChoices = document.querySelectorAll("button");
+let userChoice; // globally saved so can be accessed wherever
+let computerChoice;
+let result; 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id; // clicking button gets id
     userChoiceDisplay.innerHTML = userChoice;
     getComputerChoice();
+    playRound();
 }))
 
 // randomly generates computer choice
@@ -19,43 +21,41 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     
     if (randomNumber === 1){
-        computerChoice = "Rock";
+        computerChoice = "rock";
     }
     if (randomNumber === 2){
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
     if (randomNumber === 3){
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
     computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-
-function game() {
-    function playRound (userChoice, computerChoice) {
-        userChoice = userChoice.toLowerCase();
+function playRound() {
+    // userChoice = userChoice.toLowerCase();
+    
+    if (userChoice === computerChoice) {
+        result = "It's a tie!";
+    } if (userChoice === "rock" && computerChoice === "scissors") {
+        result = "You win! Rock beats scissors!";
         
-        if (userChoice === computerChoice) {
-            roundResult.textContent = "It's a tie!";
-        }else if (userChoice == "rock" && computerChoice == "scissors") {
-            roundResult.textContent = "You win! Rock beats scissors!";
-            return "Success!"
-        }else if (userChoice == "rock" && computerChoice == "paper") {
-            roundResult.textContent = "You lose! Paper beats rock!";
-            return "Epic fail"
-        }else if (userChoice == "paper" && computerChoice == "scissors") {
-            roundResult.textContent = "You lose! Scissors beats paper!";
-            return "Epic fail"
-        }else if (userChoice == "paper" && computerChoice == "rock") {
-            roundResult.textContent = "You win! Paper beats rock!";
-            return "Success!"
-        }else if (userChoice == "scissors" && computerChoice == "paper") {
-            roundResult.textContent = "You win! Scissors beats paper!";
-            return "Success!"
-        }else if (userChoice == "scissors" && computerChoice == "rock") {
-            roundResult.textContent = "You lose! Rock beats scissors!";
-            return "Epic fail"
+    } if (userChoice === "rock" && computerChoice === "paper") {
+        result = "You lose! Paper beats rock!";
+        
+    } if (userChoice === "paper" && computerChoice === "scissors") {
+        result = "You lose! Scissors beats paper!";
+        
+    } if (userChoice === "paper" && computerChoice === "rock") {
+        result = "You win! Paper beats rock!";
+        
+    } if (userChoice === "scissors" && computerChoice === "paper") {
+        result = "You win! Scissors beats paper!";
+        
+    } if (userChoice === "scissors" && computerChoice === "rock") {
+        result = "You lose! Rock beats scissors!";
+        
     }
-    }
-
+    resultDisplay.innerHTML = result;
 }
+
