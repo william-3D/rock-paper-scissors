f4807cb7ee3360b2953c3acfba774e594488605d
Sonia William <williamlaw2017@outlook.com>
2023-04-03 10:21:55 +0800

    simplify variable names & remove playRound duplicates

--- top-rps.js
+++ top-rps.js
@@ -1,172 +1,46 @@
 sign = window.prompt("Rock, paper or scissors?");
 
-const computerSelection = document.getElementById("computer-selection")
-const playerSelection = document.getElementById("player-selection")
+const computerChoice = document.getElementById("computer-choice")
+const userChoice = document.getElementById("user-choice")
 const roundResult = document.getElementById("result")
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     
     if (randomNumber === 1){
-        computerSelection = "Rock";
+        computerChoice = "Rock";
     }
     if (randomNumber === 2){
-        computerSelection = "Paper";
+        computerChoice = "Paper";
     }
     if (randomNumber === 3){
-        computerSelection = "Scissors";
+        computerChoice = "Scissors";
     }
 }
 
 
 function game() {
-    function playRound (playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
+    function playRound (userChoice, computerChoice) {
+        userChoice = userChoice.toLowerCase();
         
-        if (playerSelection === computerSelection) {
+        if (userChoice === computerChoice) {
             roundResult.textContent = "It's a tie!";
-        }else if (playerSelection == "rock" && computerSelection == "scissors") {
+        }else if (userChoice == "rock" && computerChoice == "scissors") {
             roundResult.textContent = "You win! Rock beats scissors!";
             return "Success!"
-        }else if (playerSelection == "rock" && computerSelection == "paper") {
+        }else if (userChoice == "rock" && computerChoice == "paper") {
             roundResult.textContent = "You lose! Paper beats rock!";
             return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "scissors") {
+        }else if (userChoice == "paper" && computerChoice == "scissors") {
             roundResult.textContent = "You lose! Scissors beats paper!";
             return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "rock") {
+        }else if (userChoice == "paper" && computerChoice == "rock") {
             roundResult.textContent = "You win! Paper beats rock!";
             return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "paper") {
+        }else if (userChoice == "scissors" && computerChoice == "paper") {
             roundResult.textContent = "You win! Scissors beats paper!";
             return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "rock") {
-            roundResult.textContent = "You lose! Rock beats scissors!";
-            return "Epic fail"
-    }
-    }
-    
-    function playRound (playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
-        
-        if (playerSelection === computerSelection) {
-            roundResult.textContent = "It's a tie!";
-        }else if (playerSelection == "rock" && computerSelection == "scissors") {
-            roundResult.textContent = "You win! Rock beats scissors!";
-            return "Success!"
-        }else if (playerSelection == "rock" && computerSelection == "paper") {
-            roundResult.textContent = "You lose! Paper beats rock!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "scissors") {
-            roundResult.textContent = "You lose! Scissors beats paper!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "rock") {
-            roundResult.textContent = "You win! Paper beats rock!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "paper") {
-            roundResult.textContent = "You win! Scissors beats paper!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "rock") {
-            roundResult.textContent = "You lose! Rock beats scissors!";
-            return "Epic fail"
-    }
-    }
-    function playRound (playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
-        
-        if (playerSelection === computerSelection) {
-            roundResult.textContent = "It's a tie!";
-        }else if (playerSelection == "rock" && computerSelection == "scissors") {
-            roundResult.textContent = "You win! Rock beats scissors!";
-            return "Success!"
-        }else if (playerSelection == "rock" && computerSelection == "paper") {
-            roundResult.textContent = "You lose! Paper beats rock!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "scissors") {
-            roundResult.textContent = "You lose! Scissors beats paper!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "rock") {
-            roundResult.textContent = "You win! Paper beats rock!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "paper") {
-            roundResult.textContent = "You win! Scissors beats paper!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "rock") {
-            roundResult.textContent = "You lose! Rock beats scissors!";
-            return "Epic fail"
-    }
-    }
-    function playRound (playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
-        
-        if (playerSelection === computerSelection) {
-            roundResult.textContent = "It's a tie!";
-        }else if (playerSelection == "rock" && computerSelection == "scissors") {
-            roundResult.textContent = "You win! Rock beats scissors!";
-            return "Success!"
-        }else if (playerSelection == "rock" && computerSelection == "paper") {
-            roundResult.textContent = "You lose! Paper beats rock!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "scissors") {
-            roundResult.textContent = "You lose! Scissors beats paper!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "rock") {
-            roundResult.textContent = "You win! Paper beats rock!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "paper") {
-            roundResult.textContent = "You win! Scissors beats paper!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "rock") {
-            roundResult.textContent = "You lose! Rock beats scissors!";
-            return "Epic fail"
-    }
-    }
-    function playRound (playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
-        
-        if (playerSelection === computerSelection) {
-            roundResult.textContent = "It's a tie!";
-        }else if (playerSelection == "rock" && computerSelection == "scissors") {
-            roundResult.textContent = "You win! Rock beats scissors!";
-            return "Success!"
-        }else if (playerSelection == "rock" && computerSelection == "paper") {
-            roundResult.textContent = "You lose! Paper beats rock!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "scissors") {
-            roundResult.textContent = "You lose! Scissors beats paper!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "rock") {
-            roundResult.textContent = "You win! Paper beats rock!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "paper") {
-            roundResult.textContent = "You win! Scissors beats paper!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "rock") {
-            roundResult.textContent = "You lose! Rock beats scissors!";
-            return "Epic fail"
-    }
-    }
-    function playRound (playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
-        
-        if (playerSelection === computerSelection) {
-            roundResult.textContent = "It's a tie!";
-        }else if (playerSelection == "rock" && computerSelection == "scissors") {
-            roundResult.textContent = "You win! Rock beats scissors!";
-            return "Success!"
-        }else if (playerSelection == "rock" && computerSelection == "paper") {
-            roundResult.textContent = "You lose! Paper beats rock!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "scissors") {
-            roundResult.textContent = "You lose! Scissors beats paper!";
-            return "Epic fail"
-        }else if (playerSelection == "paper" && computerSelection == "rock") {
-            roundResult.textContent = "You win! Paper beats rock!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "paper") {
-            roundResult.textContent = "You win! Scissors beats paper!";
-            return "Success!"
-        }else if (playerSelection == "scissors" && computerSelection == "rock") {
+        }else if (userChoice == "scissors" && computerChoice == "rock") {
             roundResult.textContent = "You lose! Rock beats scissors!";
             return "Epic fail"
     }
